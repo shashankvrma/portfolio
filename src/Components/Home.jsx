4da0ded678d70b02cf3fc8bbbe91387617a60f3a
shashankvrma <shashankvrma@gmail.com>
2024-06-01 15:10:26 -0500

    fixed contact styling

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -6,9 +6,9 @@ const Home = () => {
     <div name='home' className='w-full h-screen bg-[#0a192f]'>
        {/* Container */}
        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-          <p className='text-pink-600'>Hi, My Name is </p>
+          <p className='text-pink-600'>Hi, I am </p>
           <h1 className='text-4xl sm:text-7xl text-bold text-[#ccd6f6]'>Shashank Verma</h1>
-          <h2 className='text-4xl sm:text-7xl text-bold text-[#8892b0]'>I/m a Full Stack Developer</h2>
+          <h2 className='text-4xl sm:text-7xl text-bold text-[#8892b0]'>I'm a Full Stack Developer</h2>
           <p className='text-[#8892b0] max-w-[700px] py-4'>I specialize in Java, Spring Boot, React, MySQL ....</p>
        
         <div>
