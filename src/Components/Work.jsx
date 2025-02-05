2a5542dc45b07008197c3f196c7655073a92fc35
shashankvrma <shashankvrma@gmail.com>
2025-02-04 21:37:15 -0600

    added project

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import WorkImg from '../assets/projects/workImg.jpeg'
+import Project1 from '../assets/projects/project1.png'
 
 const Work = () => {
   return (
@@ -12,18 +13,18 @@ const Work = () => {
 
 
         <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-          <div style = {{ backgroundImage:`url(${WorkImg})`}} 
+          <div style = {{ backgroundImage:`url(${Project1})`}} 
           className='shadow-lg bg-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
             {/* Hover Effect */}
             <div className='opacity-0 group-hover:opacity-100'>
               <span className='text-2xl font-bold text-wide tracking-wider'>
-                  React JS Application
+                  AlmaMingle
               </span>
               <div className='pt-8 text-center'>
-                <a href="">
+                <a href="https://se-alma-mingle-18js.vercel.app" target="blank">
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                 </a>
-                <a href="">
+                <a href="https://github.com/kamaliniPonnuru/WallStreetDeves_SEProject/tree/main/source%20code/Phase%203" target="blank">
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                 </a>
               </div>
